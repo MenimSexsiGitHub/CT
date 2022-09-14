8632cf7be372af5639d94f8f1e363e4208b25bdb
EK <gm.testim@gmail.com>
2022-09-14 17:32:03 -0400

    Small update in appmanager

--- src/main/java/com/sn/core/buildDriver/AppManager.java
+++ src/main/java/com/sn/core/buildDriver/AppManager.java
@@ -56,7 +56,7 @@ public class AppManager {
                 EdgeOptions edgeOptions = new EdgeOptions();
                 wd = new EdgeDriver(edgeOptions);
             } else if (browser.equalsIgnoreCase("CHROME")) {
-                System.setProperty("webdriver.chrome.drive", "./chromedriver");
+                System.setProperty("webdriver.chrome.driver", "./driver/chromedriver");
 //                Map<String, Object> chromePrefs = new HashMap<String, Object>();
 //                chromePrefs.put("profile.default_content_settings.popups", 0);
 //                chromePrefs.put("profile.default_content_setting_values.notifications", 2);
